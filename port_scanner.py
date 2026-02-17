c4aa98a6e031682ffb87dd18a694869f0499db7d
bipinlamsal8 <lamsalking9851@gmail.com>
2026-02-17 20:41:09 +0545

    created a class to store port results, adn also functions init and display

--- port_scanner.py
+++ port_scanner.py
@@ -1,2 +1,16 @@
 import socket
 import time 
+
+
+class PortResult:
+    # result of port scanner is stored in this keyword class
+    
+    def __init__(self, port, status, service, timestamp):
+        self.port = port
+        self.status = status
+        self.service = service
+        self.timestamp = timestamp
+
+    def display(self):
+        time_str = time.strftime('%H:%M:%S', time.localtime(self.timestamp))
+        return f"Port {self.port} ({self.service}) : {self.status} - Scanned at {time_str}"
