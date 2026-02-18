60fecead484f100106046c953a5d226aa0b19273
bipinlamsal8 <lamsalking9851@gmail.com>
2026-02-18 22:26:56 +0545

    Invalid port numbers were kept for testing the validate_port function

--- test_scanner.py
+++ test_scanner.py
@@ -1,7 +1,9 @@
 import unittest 
 from port_scanner import validate_port
 class TestScanner(unittest.TestCase): 
-    def test_validate_port(self): #
+    def test_validate_port(self):
         self.assertTrue(validate_port(80)) 
         self.assertTrue(validate_port(1)) 
         self.assertTrue(validate_port(65535))
+        self.assertFalse(validate_port(0))
+        self.assertFalse(validate_port(65536))
