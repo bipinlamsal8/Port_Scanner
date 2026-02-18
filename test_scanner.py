30121838bbc0a74a41f2210023e367f93b7fe90b
bipinlamsal8 <lamsalking9851@gmail.com>
2026-02-18 22:24:17 +0545

    A function is created

--- test_scanner.py
+++ test_scanner.py
@@ -1,3 +1,5 @@
 import unittest 
 from port_scanner import validate_port
-class TestScanner(unittest.TestCase):
+class TestScanner(unittest.TestCase): 
+    def test_validate_port(self): 
+        self.assertTrue(validate_port(80)) 
