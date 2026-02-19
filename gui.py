6c88d957680d5bf2e2f4f630e780f9349806562c
bipinlamsal8 <lamsalking9851@gmail.com>
2026-02-19 23:31:36 +0545

    a function start_scan() was added for validating and starting the scan when user clicks startt

--- gui.py
+++ gui.py
@@ -1,2 +1,10 @@
 import tkinter as tk
 from tkinter import messagebox, scrolledtext, filedialog
+from port_scanner import scan_single_port, validate_ip, validate_port, scan_port_range, PortResult
+import time
+
+def start_scan():
+
+    if not validate_ip(target):
+        messagebox.showerror("Error", "Invalid IP address")
+        return
